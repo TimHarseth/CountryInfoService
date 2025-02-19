695130c9522ac234f5c7a903eb298c8b18424e22
TimHarseth <timhar@ntnu.no>
2025-02-19 23:56:17 +0100

    setting up main

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
+	"assignment-1/internal/handler"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	r := mux.NewRouter()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	r.HandleFunc("/countryinfo/v1/info/{two_letter_country_code}", handler.GetInfo)
+	//http.HandleFunc("/countryinfo/v1/population/{:two_letter_country_code}{?limit={:startYear-endYear}}", handler.population)
+	//http.HandleFunc("/countryinfo/v1/status/", handler.status)
+
+	fmt.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
