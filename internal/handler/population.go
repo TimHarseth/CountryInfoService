a1b7e0f61590cec0edd30b89bf0d48ca7678d246
TimHarseth <timhar@ntnu.no>
2025-02-20 22:00:44 +0100

    done with population, bit ugly code but works as intended

--- internal/handler/population.go
+++ internal/handler/population.go
@@ -1,20 +1,16 @@
 package handler
 
 import (
+	"assignment-1/internal/api"
+	"assignment-1/internal/models"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
-type population_info struct {
-	PopulationValues []map[string]int `json:"populationCounts"`
-	// fixthis
-}
-
-func getPopulation(w http.ResponseWriter, r *http.Request) {
+func GetPopulation(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	country_code := vars["two_letter_country_code"]
 
@@ -24,67 +20,58 @@ func getPopulation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res, err := http.Get("http://129.241.150.113:8080/v3.1/alpha/" + country_code + "?fields=name")
+	// Fetch, decode and return struct of Country API
+	countryInfo, err := api.GetCountryApi(country_code)
 	if err != nil {
-		// Log the error and return an internal server error response
-		log.Printf("Error making HTTP request: %v", err)
-		http.Error(w, "Internal Server Error: Unable to fetch data", http.StatusInternalServerError)
-		return
-	}
-	defer res.Body.Close() // Ensure the response body is closed
-
-	// Handle 404 Not Found response from the first API
-	if res.StatusCode == 404 {
-		http.Error(w, "Country not found", http.StatusNotFound)
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
-	var getName string
-	// Decode the country name  from the first API response
-	if err := json.NewDecoder(res.Body).Decode(&getName); err != nil {
-		log.Printf("Error decoding response: %v", err)
-		http.Error(w, "Internal Server Error: Unable to decode country data", http.StatusInternalServerError)
-		return
-	}
-
-	// Fetch city information from the second API
-	url := "http://129.241.150.113:3500/api/v0.1/countries/population"
-	method := "POST"
-
-	// Create the payload for the second API request
-	payload := strings.NewReader(fmt.Sprintf(`{"country": "%s"}`, getName))
-
-	// Create a new HTTP request for the second API
-	req, err := http.NewRequest(method, url, payload)
+	// Fetch, decode and return struct of Population API
+	populationInfo, err := api.GetPopulationAPI(countryInfo.Name.Common)
 	if err != nil {
-		log.Printf("Error creating request: %v", err)
-		http.Error(w, "Internal Server Error: Unable to fetch data", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
-	req.Header.Set("Content-Type", "application/json") // Set the content type header
+	var limitedPopulation []models.PopulationValue
+	yearLimit := r.URL.Query().Get("limit")
+	if yearLimit != "" {
+		years := strings.Split(yearLimit, "-")
+		if len(years) == 2 {
+			startYear, _ := strconv.Atoi(years[0])
+			endYear, _ := strconv.Atoi(years[1])
 
-	// Send the request to the second API
-	client := &http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Failed to fetch population", http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close() // Ensure the response body is closed
+			for i := 0; i < len(populationInfo.Data.PopulationValues); i++ {
+				if populationInfo.Data.PopulationValues[i].Year >= startYear && populationInfo.Data.PopulationValues[i].Year <= endYear {
+					limitedPopulation = append(limitedPopulation, populationInfo.Data.PopulationValues[i])
+				}
+			}
 
-	// Handle non-200 status codes from the second API
-	if response.StatusCode != http.StatusOK {
-		log.Printf("Unexpected status code from second API: %d", response.StatusCode)
-		http.Error(w, "Internal Server Error: Unable to fetch population", http.StatusInternalServerError)
-		return
-	}
+		}
 
-	// Decode the city information from the second API response
-	var populationInfo population_info
-	if err := json.NewDecoder(response.Body).Decode(&cityInfo); err != nil {
-		log.Printf("Error decoding response: %v", err)
-		http.Error(w, "Internal Server Error: Unable to decode population data", http.StatusInternalServerError)
-		return
+		if len(years) != 2 {
+			http.Error(w, "Invalid limit", http.StatusNotFound)
+		}
+
+	} else {
+		limitedPopulation = populationInfo.Data.PopulationValues
 	}
 
+	// calculate mean
+	var sum int
+	for i := 0; i < len(limitedPopulation); i++ {
+		sum += limitedPopulation[i].Value
+	}
+	sum = sum / len(limitedPopulation)
+
+	FinalPopulation := models.FinalPopulation{
+		Mean:   sum,
+		Values: limitedPopulation,
+	}
+
+	// Write the combined information as a JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	out, _ := json.Marshal(FinalPopulation)
+	w.Write(out)
 }
