fc3cbe14ab4063702e27203edfda0fd7d60101b9
TimHarseth <timhar@ntnu.no>
2025-02-25 06:33:28 +0100

    added path to the default api of CountriesNow and RestCountry

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -5,3 +5,7 @@ const CountryApiPath = "http://129.241.150.113:8080/v3.1/alpha/"
 const CityApiPath = "http://129.241.150.113:3500/api/v0.1/countries/cities"
 
 const PopulationApiPath = "http://129.241.150.113:3500/api/v0.1/countries/population"
+
+const CountriesnowHealthApiPath = "http://129.241.150.113:8080"
+
+const RestcountriesHealthApiPath = "http://129.241.150.113:3500"
