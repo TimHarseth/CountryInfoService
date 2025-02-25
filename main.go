9a7fd0cfc2efeeb94b271b0fc85a15c36aeeee92
TimHarseth <timhar@ntnu.no>
2025-02-25 06:34:09 +0100

    Added paths to status, default root and default path of info and population

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	handler.Starttime = time.Now() // start timer when application starts for Uptime in status
 	r := mux.NewRouter()
 
 	r.HandleFunc("/countryinfo/v1/info/{two_letter_country_code}", handler.GetInfo)
 	r.HandleFunc("/countryinfo/v1/population/{two_letter_country_code}", handler.GetPopulation)
-	//http.HandleFunc("/countryinfo/v1/status/", handler.status)
+	r.HandleFunc("/countryinfo/v1/status/", handler.GetStatus)
+	r.HandleFunc("/countryinfo/v1/", handler.Root)
+	r.HandleFunc("/countryinfo/v1/info/", handler.DefaultInfo)
+	r.HandleFunc("/countryinfo/v1/population/", handler.DefaultPopulation)
 
 	fmt.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
